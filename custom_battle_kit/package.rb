0ac0ae5ce44fa959affb64fb177fadc790dbcb83
BastienDuplessier <zangther@gmail.com>
2015-08-19 17:21:51 +0200

    [CustomBattleKit] Add base files

---
+++ custom_battle_kit/package.rb
@@ -0,0 +1,7 @@
+Package.new(
+	name: 'custom-battle-kit',
+	version: vsn(0, 0, 0),
+	authors: {'Zangther' => 'zangther@gmail.com'},
+	components: ['script.rb'],
+	description: 'Add basic tools to create easily custom battle scenes.'
+)
