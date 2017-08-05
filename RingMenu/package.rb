3e856b206eaaac8df4cf99cda4f1c34ac58f3951
BastienDuplessier <zangther@gmail.com>
2017-08-05 23:53:53 +0200

    [RingMenu] Update version to 1.5.1

--- RingMenu/package.rb
+++ RingMenu/package.rb
@@ -1,6 +1,6 @@
 Package.new(
 	name: 'ring-menu',
-	version: vsn(1, 5, 0),
+	version: vsn(1, 5, 1),
 	authors: {'Zangther' => 'zangther@gmail.com'},
 	components: ['script.rb'],
 	description: 'Change base menu for a brand new ring shaped menu.'
